e11de97acea97e3f657cc56334bbd5d96d551585
Mohamed Wael <mwael2915@gmail.com>
2023-08-26 19:44:41 +0300

    LCD Module Driver Added

---
+++ HAL/Inc/LCD_Private.h
@@ -0,0 +1,13 @@
+/*
+ *@file		:	LCD_Private.h
+ *@author	: 	Mohamed Wael
+ *@brief	:	Main Private file for LCD
+ */
+
+#ifndef HAL_LCD_INC_LCD_PRIVATE_H_
+#define HAL_LCD_INC_LCD_PRIVATE_H_
+
+#define LCD_MASK 			0b1111
+#define COMMAND_MASK		0x0f
+
+#endif /* HAL_LCD_INC_LCD_PRIVATE_H_ */
