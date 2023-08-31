274f9331f8218582583a7d54c6f2ed5c1a9e5333
Ibrahim Mohamed <ibrahim464114@gmail.com>
2023-08-31 14:46:19 +0300

    Adding Main Application

--- APP/main.c
+++ APP/main.c
@@ -12,15 +12,21 @@
 
 #include <stdint.h>
 
-#include "../HAL/Inc/LCD_Interface.h"
-#include "../HAL/Inc/LED_Interface.h"
-#include "../HAL/Inc/BUZZER_Interface.h"
+#include "../LIBRARY/ErrTypes.h"
+#include "../LIBRARY/BitMath.h"
+#include "../LIBRARY/STM32F103xx.h"
+#include "../MCAL/Inc/SYSTICK_Interface.h"
+
+#include "../Service/Inc/Service.h"
 
 /*****************************************************************************/
 /*******************   GLOBAL VARIABLES SECTION    ***************************/
 /*****************************************************************************/
 
+extern volatile uint8_t flag ;
 
+
+volatile uint8_t RECEIVED = 0 ;
 /*****************************************************************************/
 /************************   MACROS SECTION    ********************************/
 /*****************************************************************************/
@@ -38,9 +44,42 @@
 /*****************************************************************************/
 int main(void)
 {
-    /* Loop forever */
+
+    /* Initialization Clock */
+	Clock_Init( ) ;
+
+	/* Receive Time form Nucleo*/
+	Recive_Time();
+
+	/* Loop forever */
 	while(1)
 	{
+
+        /* Check if data is receive */
+		if(flag ==1)
+		{
+			/* Check if receive is repeated */
+			if( RECEIVED == 0 )
+			{
+				   /* Receive Time form Nucleo*/
+				   Recive_Time( ) ;
+				   /* to perevent calling the Function more than one time without receiving data */
+				   RECEIVED = 1 ;
+			}
+			/* Display Date on LCD */
+			Display_Date();
+			/* Calculate the Time */
+			Count_Time();
+			/* Delay 1 sec */
+			SYSTICK_u8Delay_ms(1000);
+			/* Display Time on LCD */
+			Display_Time();
+
+
+
+
+		}
+
 
 	}
 }
@@ -51,3 +90,9 @@ int main(void)
 /*****************************************************************************/
 
 
+
+void SPI_CALL_BACK(void)
+{
+	flag=1;
+   RECEIVED = 0 ;
+}
