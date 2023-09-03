61f8a7abfeb287f04251ecc326ff4efeb7f840f5
Ibrahim Mohamed <ibrahim464114@gmail.com>
2023-09-03 12:32:19 +0300

    Solve Receiving from SPI

--- APP/main.c
+++ APP/main.c
@@ -20,8 +20,10 @@
 extern volatile uint8_t flag ;
 
 volatile uint8_t RECEIVED = 0 ;
- 
+
 uint8_t  DATA_ReceivedFromSPI = 0 ;
+
+extern uint8_t Received[7];
 
 /*****************************************************************************/
 /***********************   MAIN CODE SECTION    ******************************/
@@ -41,37 +43,37 @@ int main(void)
 	Interrupts_Init( ) ;
 
 	/* Receive Using SPI with Interrupt */
-	//Receive_withInterrupt( ) ;
-	
-	/* Receive Time form Nucleo*/
-	Recive_Time();
+	Receive_withInterrupt( ) ;
 
 
 	/* Loop forever */
 	while(1)
 	{
- /* Check if data is receive */
+		/* Check if data is receive */
 		if(flag ==1)
 		{
 			/* Check if receive is repeated */
 			if( RECEIVED == 0 )
 			{
-				   /* Receive Time form Nucleo*/
-				   Recive_Time( ) ;
-				   /* to perevent calling the Function more than one time without receiving data */
-				   RECEIVED = 1 ;
+				/* Receive Time form Nucleo*/
+				Receive_withInterrupt( ) ;
+				/* to perevent calling the Function more than one time without receiving data */
+				RECEIVED = 1 ;
 			}
 			/* Display Date on LCD */
 			Display_Date();
 			/* Calculate the Time */
 			Count_Time();
 			/* Delay 1 sec */
-			SYSTICK_u8Delay_ms(1000);
+			DELAY_1S();
 			/* Display Time on LCD */
 			Display_Time();
 
+		}
 	}
 }
+
+
 
 /*****************************************************************************/
 /***********************      ISRs SECTION      ******************************/
@@ -79,7 +81,7 @@ int main(void)
 
 void SPI1_CallBack( void )
 {
-	if( DATA_ReceivedFromSPI == RED_LED_CODE )
+	if( Received[0] == RED_LED_CODE )
 	{
 		/* Turn RED LED ON */
 		TURN_ON_LED( ) ;
@@ -90,7 +92,6 @@ void SPI1_CallBack( void )
 		RECEIVED = 0 ;
 	}
 }
-
 
 
 
